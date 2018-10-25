de16bb14629a4eeb1243b13555a77452d734932b
Ben Francisco <bfrancisco.home@gmail.com>
2018-10-25 17:13:50 -0500

    initial commit; shortened syntax module

---
+++ vagrant/Clip4/assets/dbinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbpassword = "Songohan1";
+$dbusername = "PHPFundamentals";
+$dbserver = "localhost";
+$dbname = "PHPFundamentals";
+
+//connection object variable
+$connection = new mysqli($dbserver, $dbusername, $dbpassword, $dbname, 3306);
+
+if($connection->connect_errno)
+{
+    exit("Database Connection has failed. Reason ".$connection->connect_error);
+}
+
+?>
---
+++ vagrant/Clip4/index.php
@@ -0,0 +1,66 @@
+<?php
+
+require 'assets/dbinfo.php';
+
+$query = "SELECT first_name, last_name, pen_name FROM Authors ORDER BY first_name";
+$resultObject = $connection->query($query);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Fundamentals</title>
+		<link href="assets/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+		<div id="Header">
+			<img src="assets/Dickens_Gurney_head.jpg" border="0" alt="">
+			<h2>
+				Join Our Literature Mailing List
+			</h2>
+		</div>        
+        <div id="Body">
+            <form method="get" action="final.php" >
+                <div>
+                    <label>Favorite Author:</label>
+                    <select name="author">
+                        <!--using shorthand syntax for while loop, can use regular if needed-->
+                        <?php while($row = $resultObject->fetch_assoc()) : ?>
+                        <!--using shorthand for php start line, inserting the value of id,firstname, lastname from each row -->
+                        <option value="<?=$row['id']?>"><?=$row['first_name']?> <?=$row['last_name']?></option>
+                        <?php endWhile ?>
+                    </select>
+                </div>		
+                <div class="multiple">
+                    <label>Favorite Century:</label>
+                    17th Century <input type="checkbox" name="century" value="17th">
+                    18th Century <input type="checkbox" name="century" value="18th"> 
+                    19th Century <input type="checkbox" name="century" value="19th"> 
+                </div>
+                <div>
+                    <label>Comments:</label>
+                    <textarea name="comments"></textarea>
+                </div>
+                <div>
+                    <label>Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label>E-mail Address:</label>
+                    <input type="text" name="email" />
+                </div>
+                <div  class="multiple">
+                    <label>Receive Newsletter:</label>
+                    Yes <input type="radio" name="newsletter" value="no">
+                    No <input type="radio" name="newsletter" value="yes">
+                </div>
+                <div class="multiple">
+                    <label>&nbsp;</label>
+                    <input type="submit" name="submit" value="Join Mailing List">
+                </div>
+            </form>
+        </div>
+	</body>
+</html>
